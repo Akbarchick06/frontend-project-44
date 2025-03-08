17a32981299b99c932d8e092cc2fe75bba920d7b
Akbarchick06 <164862450+Akbarchick06@users.noreply.github.com>
2025-03-08 10:45:29 +0300

    add brain.calc

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 import { getUserName } from "../src/cli.js";
 import gameRandomNumber from "./brain-even.js";
+import getRandomOper from "./brain.calc.js";
 
 console.log("Welcome to the Brain Games!");
 
 const name = getUserName()
 console.log(`Hello, ${name}!`);
 
+const results = getRandomOper(name);
+console.log(results); 
 
 const result = gameRandomNumber(name)
 console.log(result);
---
+++ bin/brain.calc.js
@@ -0,0 +1,36 @@
+import readlineSync from "readline-sync";
+
+const getRandomOper = (userName) => {
+  let i = 0;
+  while (i < 3) {
+    const num1 = Math.floor(Math.random() * 10);  // Уменьшаем для удобства
+    const num2 = Math.floor(Math.random() * 10);  // Уменьшаем для удобства
+    const operationsArray = ['+', '-', '*'];
+    const randomOperationIndex = Math.floor(Math.random() * operationsArray.length);
+    const selectedOperation = operationsArray[randomOperationIndex];
+    const expression = `${num1} ${selectedOperation} ${num2}`;
+
+    let correctAnswer;
+    switch (selectedOperation) { 
+      case '+': correctAnswer = num1 + num2; break;
+      case '-': correctAnswer = num1 - num2; break;
+      case '*': correctAnswer = num1 * num2; break;
+      default: correctAnswer = null;
+    }
+
+    console.log(`Question: ${expression}`);
+    const userResponse = parseInt(readlineSync.question(`Your choice: `), 10);
+
+    if (userResponse === correctAnswer) {
+      console.log(`Correct! ${userName}`);
+      i++; 
+    } else {
+      return `No correct! ${userName}. Correct answer was ${correctAnswer}`;
+    }
+  }
+  return `Congratulations, ${userName}! You won!`;
+};
+
+
+export default getRandomOper;
+
