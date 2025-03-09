c581e004a199e1a3babc556e5fa7636d47ea9213
Akbarchick06 <164862450+Akbarchick06@users.noreply.github.com>
2025-03-09 15:11:47 +0300

    add file

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,17 +3,21 @@ import { getUserName } from "../src/cli.js";
 import gameRandomNumber from "./brain-even.js";
 import getRandomOper from "./brain.calc.js";
 import gameNOD from "./brain-gcd.js";
+import gameProgression from "./brain-progression.js"
 
 console.log("Welcome to the Brain Games!");
 
 const name = getUserName()
 console.log(`Hello, ${name}!`);
 
-const results = getRandomOper(name);
-console.log(results); 
+const result1 = getRandomOper(name);
+console.log(result1); 
 
-const result = gameRandomNumber(name);
-console.log(result);
+const result2 = gameRandomNumber(name);
+console.log(result2);
 
-const resulte = gameNOD(name);
-console.log(resulte);
+const result3 = gameNOD(name);
+console.log(result3);
+
+const result4 = gameProgression(name);
+console.log(result4);
